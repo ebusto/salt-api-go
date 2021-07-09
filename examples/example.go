a150b51e6e7a7550b473959cb673af0b5da1f88d
Eric Busto <ebusto@nvidia.com>
2021-07-09 13:02:18 -0700

    Include an example of how to run an arbitrary command and decode the response into a custom data structure.

--- examples/example.go
+++ examples/example.go
@@ -22,6 +22,8 @@ func main() {
 		log.Fatal(err)
 	}
 
+	defer c.Logout(ctx)
+
 	// Display a list of all minions.
 	c.Minions.All(ctx, printMinion)
 
@@ -31,7 +33,16 @@ func main() {
 	// Display a list of all jobs.
 	c.Jobs.All(ctx, printJob)
 
-	c.Logout(ctx)
+	// Ping all minions.
+	c.Ping(ctx, "*", printPong)
+
+	cmd := salt.Command{
+		Client:   "local",
+		Function: "disk.usage",
+		Target:   "*salt*",
+	}
+
+	c.Run(ctx, &cmd, printUsage)
 }
 
 func printMinion(id string, grains salt.Response) error {
@@ -42,6 +53,33 @@ func printMinion(id string, grains salt.Response) error {
 
 func printJob(id string, job salt.Response) error {
 	log.Println(id, job.Get("User"), job.Get("Target"), job.Get("Function"), job.Get("Arguments"))
+
+	return nil
+}
+
+func printPong(id string, ok bool) error {
+	log.Println(id, ok)
+
+	return nil
+}
+
+func printUsage(id string, response salt.Response) error {
+	// The return from Salt for disk.uage unfortunately encodes all values as
+	// strings, even if they're integers, hence the `json:"<field>,string"`.
+	var usage map[string]struct {
+		Available  int    `json:"available,string"`
+		Capacity   string `json:"capacity"`
+		Filesystem string `json:"filesystem"`
+		Used       int    `json:"used,string"`
+	}
+
+	if err := response.Decode(&usage); err != nil {
+		log.Fatal(err)
+	}
+
+	for mount, info := range usage {
+		log.Println(id, mount, info.Filesystem, info.Used)
+	}
 
 	return nil
 }
